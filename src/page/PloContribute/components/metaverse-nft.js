f869d985fc72f9e819643fef15b39c460b6b687f
JayJay1024 <lygm1062@gmail.com>
2022-11-25 17:55:03 +0800

    fix subquery endpoint

--- src/page/PloContribute/components/metaverse-nft.js
+++ src/page/PloContribute/components/metaverse-nft.js
@@ -301,7 +301,7 @@ const MetaverseNFT = ({ myTotalContribute, currentAccount }) => {
 };
 
 const client = new ApolloClient({
-  uri: "https://api.subquery.network/sq/JayJay1024/crab-nft-kusama",
+  uri: "https://api.subquery.network/sq/JayJay1024/crab-nft",
   cache: new InMemoryCache(),
 });
 
